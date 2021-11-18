fcf0e89a912ffe6adbf926972c317d9642a5c4e4
Jabca <pavlo.sterlon@gmail.com>
2021-11-18 23:14:11 +0300

    bug fix

--- lib/data_transform/parse_alphavantage.py
+++ lib/data_transform/parse_alphavantage.py
@@ -10,7 +10,7 @@ def yearly_profits_w_dividends(data: dict, years_count=5) -> list:
     for i in range(0, n - 12, 12):
         this_period = float(data[time_stamps[i]]["5. adjusted close"])
         prev_period = float(data[time_stamps[i+12]]["5. adjusted close"])
-        profit = (this_period + prev_period) / prev_period
+        profit = (this_period - prev_period) / prev_period
         yearly_profits.append({"profit": profit, "time": time_stamps[i]})
 
     return yearly_profits
