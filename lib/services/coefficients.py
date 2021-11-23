1d31036c0dc651c086df9057b2ebe0d9fa9acd91
Jabca <pavlo.sterlon@gmail.com>
2021-11-23 18:41:01 +0300

    Sharpe coefficient

--- lib/services/coefficients.py
+++ lib/services/coefficients.py
@@ -2,6 +2,8 @@ from lib.math_functions.standard_deviation import standard_deviation
 from lib.math_functions.averages import average_geometrical, average_arithmetic
 from lib.data_transform.abnormal_returns import get_abnormal_returns
 
+us_bonds_profits = 0.03
+
 
 def variation_coefficient(yearly_profits: list) -> float:
     numerator = standard_deviation(yearly_profits)
@@ -12,6 +14,10 @@ def variation_coefficient(yearly_profits: list) -> float:
 def information_coefficient(profits_y: list, benchmark_profits_y: list, profits_d: list, benchmark_profits_d: list) -> float:
     numerator = average_geometrical(profits_y) - average_geometrical(benchmark_profits_y)
     denominator = standard_deviation(get_abnormal_returns(profits_d, benchmark_profits_d)) * 252 ** 0.5
-
     return numerator / denominator
 
+
+def sharpe_coefficient(profits_y: list) -> float:
+    numerator = average_geometrical(profits_y) - us_bonds_profits
+    denominator = standard_deviation(profits_y)
+    return numerator / denominator
