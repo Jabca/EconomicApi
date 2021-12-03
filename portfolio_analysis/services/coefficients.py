cafab7cd92d05fd1ef026e2367260a6edb9984f8
Jabca <pavlo.sterlon@gmail.com>
2021-12-03 20:38:57 +0300

    minor bug fix

--- portfolio_analysis/services/coefficients.py
+++ portfolio_analysis/services/coefficients.py
@@ -3,7 +3,6 @@ from portfolio_analysis.math_functions.averages import average_geometrical, aver
 from portfolio_analysis.data_transform.abnormal_returns import get_abnormal_returns
 from portfolio_analysis.data_transform.downside import get_downsides
 
-us_bonds_yearly_profits = 0.03
 
 """This file contain functions that calculate coefficients"""
 
@@ -28,28 +27,28 @@ def information_ratio(profits_y: list, benchmark_profits_y: list, profits_d: lis
     return numerator / denominator
 
 
-def sharpe_ratio(profits_y: list) -> float:
+def sharpe_ratio(profits_y: list, non_risk_return=0.03) -> float:
     """calculate sharpe ratio based on array of yearly profits"""
-    numerator = average_geometrical(profits_y) - us_bonds_yearly_profits
+    numerator = average_geometrical(profits_y) - non_risk_return
     denominator = standard_deviation(profits_y)
     return numerator / denominator
 
 
-def sortino_ratio(profits_y: list, profit_d: list) -> float:
+def sortino_ratio(profits_y: list, profit_d: list, non_risk_return=0.03) -> float:
     """calculate sortino ratio based on arrays of
         share yearly profits,
         share daily profits"""
 
-    numerator = average_geometrical(profits_y) - us_bonds_yearly_profits
+    numerator = average_geometrical(profits_y) - non_risk_return
     denominator = standard_deviation(get_downsides(profit_d)) * 252 ** 0.5
     return numerator / denominator
 
 
-def treynor_ratio(profits_y: list, beta_coefficient: float):
+def treynor_ratio(profits_y: list, beta_coefficient: float, non_risk_return=0.03):
     """calculate treynor ratio based on:
             array of share yearly profits,
             share beta coefficient  """
 
-    numerator = average_geometrical(profits_y) - us_bonds_yearly_profits
+    numerator = average_geometrical(profits_y) - non_risk_return
     denominator = beta_coefficient
     return numerator / denominator
