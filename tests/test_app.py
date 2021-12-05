c72f426d74dd6811a96ee532e259eae77041900c
Jabca <pavlo.sterlon@gmail.com>
2021-12-05 16:24:35 +0300

    tests for profits

--- tests/test_app.py
+++ tests/test_app.py
@@ -7,33 +7,38 @@ from pandas import read_csv
 from pandas import to_datetime
 
 
-def scan_file(name: str, key: str) -> list:
+def scan_file(name: str, key_row: str, key_col) -> list:
     a = []
     with open(f"tests/resources/{name}", newline='') as file:
         reader = csv.DictReader(file, delimiter=';')
         for row in reader:
-            a.append({"time": row["Date"], "price": float(row[key])})
+            a.append({"time": row["Date"], key_col: float(row[key_row])})
     return a
 
 
 @pytest.fixture()
 def yearly_prices():
-    return scan_file("prices_yearly.csv", "Close")[::-1]
+    return scan_file("prices_yearly.csv", "Close", "price")[::-1]
 
 
 @pytest.fixture()
 def daily_prices():
-    return scan_file("prices_daily.csv", "Close")[::-1]
+    return scan_file("prices_daily.csv", "Close", "price")[::-1]
 
 
 @pytest.fixture()
 def sp500_yearly():
-    return scan_file("sp500_yearly.csv", "Close")[::-1]
+    return scan_file("sp500_yearly.csv", "Close", "price")[::-1]
 
 
 @pytest.fixture()
 def sp500_daily():
-    return scan_file("sp500_yearly.csv", "Close")[::-1]
+    return scan_file("sp500_yearly.csv", "Close", "price")[::-1]
+
+
+@pytest.fixture()
+def daily_profits():
+    return scan_file("profits_daily.csv", "profit", "profit")[::-1]
 
 
 @pytest.fixture()
@@ -60,12 +65,9 @@ def test_day():
     return datetime.datetime(year=2021, month=12, day=2)
 
 
-
-
-
-def compare_dictionaries(d1, d2, price_diff=1e-3):
+def compare_dictionaries(d1, d2, price_diff=1e-3, key="price"):
     for c1, c2 in zip(d1, d2):
         # print(c1, c2)
-        assert (abs(c1["price"] - c2["price"]) < price_diff)
+        assert (abs(c1[key] - c2[key]) < price_diff)
         assert (c1["time"] == c2["time"])
 
